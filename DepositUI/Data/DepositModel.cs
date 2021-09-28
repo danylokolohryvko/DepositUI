9c2b8cd0fcb76ab1bf7e5fd614276d3e325baf67
Danylo Kolohryvko <yoodenkol@gmail.com>
2021-09-28 15:32:02 +0300

    Added history and Details tabs for viewing Deposits info

--- DepositUI/Data/DepositModel.cs
+++ DepositUI/Data/DepositModel.cs
@@ -7,10 +7,14 @@ namespace DepositUI.Data
 {
     public class DepositModel
     {
+        public int Id { get; set; }
+
         public double? Amount { get; set; }
 
         public int? Term { get; set; }
 
         public double? Percent { get; set; }
+
+        public DateTime Date { get; set; }
     }
 }
